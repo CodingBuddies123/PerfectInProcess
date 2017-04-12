9f7b54d674af6b4b1292c83d0c40c246e9b6d635
sisco035 <sisco035@gmail.com>
2017-04-12 19:05:56 -0400

    Logic for visitor counter

---
+++ PerfectInProcess/PerfectInProcess/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PerfectInProcess.Models;
+
+namespace PerfectInProcess.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {            
+            Home Visitor = new Home();
+            Visitor.GetVisitorCount();
+
+            Visitor.VisitorCount += 1;
+
+            Visitor.SetVisitorCount();
+
+
+            return View("Index");
+        }
+    }
+}
---
+++ PerfectInProcess/PerfectInProcess/Models/Home.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PerfectInProcess.Models
+{
+    public class Home
+    {
+
+        public static ArrayList listOfErrors = new ArrayList();
+
+        public int VisitorCount { get; set; }
+
+
+        public void SetVisitorCount()
+        {
+            SqlConnection connection = new SqlConnection(Convert.ToString(ConfigurationManager.ConnectionStrings["DefaultConnection"]));
+
+            try
+            {
+                using (SqlCommand command = new SqlCommand("SetVisitorCount", connection))
+                {
+                    command.Parameters.Add("@VisitorCount", SqlDbType.Int).Value = VisitorCount;                   
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    command.ExecuteNonQuery(); 
+                }
+            }
+            catch (SqlException ex)
+            {
+                listOfErrors.Add(ex.Message);
+            }
+
+        }
+
+
+        public void GetVisitorCount()
+        {
+            SqlConnection connection = new SqlConnection(Convert.ToString(ConfigurationManager.ConnectionStrings["DefaultConnection"]));
+
+            try
+            {
+                using (SqlCommand command = new SqlCommand("GetVisitorCount", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;                   
+                    command.Parameters.Add("@VisitorCount", SqlDbType.Int).Direction = ParameterDirection.Output; ;
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                    VisitorCount = (int)command.Parameters["@VisitorCount"].Value;
+                }
+            }
+            catch (SqlException ex)
+            {
+                listOfErrors.Add(ex.Message);
+            }
+
+        }
+
+
+
+
+    }
+}
