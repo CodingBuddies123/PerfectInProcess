2d277ebb8a5cd8e709a5db2f1439d65fea54b3f3
houyja <houyja@gmail.com>
2017-04-12 22:39:36 -0400

    BaseController 1.0

---
+++ PerfectInProcess/PerfectInProcess/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PerfectInProcess.Controllers
+{
+    public class BaseController : Controller
+    {
+        private String Controller;
+        private String Action;
+        public BaseController(String controller, String action)
+        {
+            Controller = controller;
+            Action = action;
+        }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+
+
+        }
+    }
+}
