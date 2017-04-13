b0d20e7183aeb248e34fc21ab695098253e3275e
houyja <houyja@gmail.com>
2017-04-13 13:02:25 -0400

    BaseController 1.3
    Account Save Base Functions
    Permission Checking Logc
    Account and Permission DataModels

--- PerfectInProcess/PerfectInProcess/Controllers/BaseController.cs
+++ PerfectInProcess/PerfectInProcess/Controllers/BaseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using PerfectInProcess.Models;
 
 namespace PerfectInProcess.Controllers
 {
@@ -11,7 +12,7 @@ namespace PerfectInProcess.Controllers
     {
         private String Controller;
         private String Action;
-        private Object Account;
+        private AccountDataModel Account;
 
         public BaseController()
         {
@@ -21,11 +22,40 @@ namespace PerfectInProcess.Controllers
         protected override void Initialize(RequestContext requestContext)
         {
             base.Initialize(requestContext);
+            Load_BaseInfo();
 
+            if (!CheckPermissions())
+            {
+                //Redirect to Access Denied Page
+            }
+        }
+
+        protected void Save_BaseInfo()
+        {
+            Session["AccountModel"] = Account;
+        }
+
+        private void Load_BaseInfo()
+        {
+            try
+            {
+                if (Session["AccountModel"] == null)
+                    Account = new AccountDataModel();
+                else
+                    Account = (AccountDataModel)Session["AccountModel"];
+
+            }
+            catch
+            {
+                Account = new AccountDataModel();
+            }
+        } 
+
+        private Boolean CheckPermissions()
+        {
             string Action = this.ControllerContext.RouteData.Values["action"].ToString();
             string Controller = this.ControllerContext.RouteData.Values["controller"].ToString();
-
-
+            return Account.CheckPermissions(Controller, Action);
         }
     }
 }
---
+++ PerfectInProcess/PerfectInProcess/Models/AccountDataModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfectInProcess.Models
+{
+    public class AccountDataModel
+    {
+        List<PermissionDataModel> Permissions = new List<PermissionDataModel>();
+
+        public Boolean CheckPermissions(String Controller, String Action)
+        {
+            foreach(PermissionDataModel permission in Permissions)
+            {
+                if (permission.Action.ToLower() == Action.ToLower() && permission.Controller.ToLower() == Controller.ToLower())
+                    return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class PermissionDataModel
+    {
+        public String Controller { get; private set; }
+        public String Action { get; private set; }
+
+        public PermissionDataModel(string action, string controller)
+        {
+            Controller = controller;
+            Action = action;
+        }
+    }
+}
