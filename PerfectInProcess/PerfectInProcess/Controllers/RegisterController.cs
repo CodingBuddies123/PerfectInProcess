43a6163e1349040546780f8a83514154d312e14c
sisco035 <sisco035@gmail.com>
2017-04-14 00:39:06 -0400

    controller and viewmodel added

---
+++ PerfectInProcess/PerfectInProcess/Controllers/RegisterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PerfectInProcess.Controllers
+{
+    public class RegisterController : BaseController
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
