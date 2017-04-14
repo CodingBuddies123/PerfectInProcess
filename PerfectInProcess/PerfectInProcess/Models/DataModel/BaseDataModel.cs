67f7d805f105b6a358308123a20510792f2dfbc5
houyja <houyja@gmail.com>
2017-04-13 21:50:18 -0400

    BaseDataModel Permissions, AccountClassPartial

---
+++ PerfectInProcess/PerfectInProcess/Models/DataModel/BaseDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfectInProcess.Models.DataModel
+{
+    public class BaseDataModel
+    {
+        List<String> Errors = new List<string>();
+
+        protected void SetError(string error)
+        {
+            Errors.Add(error);
+        }
+
+        public List<string> GetError()
+        {
+            return Errors;
+        }
+
+        public void ClearError()
+        {
+            Errors.Clear();
+        }
+
+    }
+}
---
+++ PerfectInProcess/PerfectInProcess/Models/DataModel/PermissionsDataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfectInProcess.Models.DataModel
+{
+    public class PermissionsDataModel : BaseDataModel
+    {
+        public int PermissionID { get; private set; }
+        public string PermissionName { get; private set; }
+        public string PermissionGroupName { get; private set; }
+        public string Controller { get; private set; }
+        public string Action { get; private set; }
+        public Boolean Hidden { get; private set; }
+
+        public PermissionsDataModel(int permissionID, string permissionName, string permissionGroupName, string controller, string action)
+        {
+            PermissionID = permissionID;
+            PermissionName = permissionName;
+            PermissionGroupName = permissionGroupName;
+            Controller = controller;
+            Action = action;
+            Hidden = permissionGroupName.ToLower() == "hidden";
+        }
+    }
+}
