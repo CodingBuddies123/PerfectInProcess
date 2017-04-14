ef45f8f3c9f463eca8b6ea2684d137d2c1e59420
houyja <houyja@gmail.com>
2017-04-13 22:57:26 -0400

    Revert "Revert "BaseDataModel Permissions, AccountClassPartial""
    This reverts commit 66541ecf17f20477fa01319fb14d90eb9dd95ecc.

---
+++ PerfectInProcess/PerfectInProcess/Models/DataModel/AccountDataModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace PerfectInProcess.Models.DataModel
+{
+    public class AccountDataModel : BaseDataModel
+    {
+        List<PermissionsDataModel> Permissions = new List<PermissionsDataModel>();
+
+        public Boolean VerifyPermission(string controller, string action)
+        {
+            foreach(PermissionsDataModel permission in Permissions)
+            {
+                if (permission.Controller.ToLower() == controller.ToLower() && permission.Action.ToLower() == action.ToLower())
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
