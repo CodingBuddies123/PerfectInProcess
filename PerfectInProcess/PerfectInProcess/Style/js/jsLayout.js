b820a013bf7caa1819cc3eadc363f35ebf37ecc0
houyja <houyja@gmail.com>
2017-05-11 21:12:23 -0400

    UpdatedLayout V1.4 Responsive Design and Dynamic Menu

---
+++ PerfectInProcess/PerfectInProcess/Style/js/jsLayout.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function () {
+    SizeLayoutElements();
+    ResizeLogo();
+})
+
+$(window).resize(function () {
+    SizeLayoutElements();
+    ResizeLogo();
+})
+
+function toggleSecondaryMenu() {
+    $('.menu-togglesidebar-btn').toggleClass("hidden");
+    $('.w3-overlay').toggleClass("overlay-toggle-show");
+    $('.menu-secondary-container').toggleClass("hidden");
+
+    $('.PermissionGroup').addClass("hidden");
+    $('.inactive-carrot').removeClass('hidden');
+    $('.active-carrot').addClass('hidden');
+
+}
+
+function toggleSecondaryMenuGroup(x) {
+    var id = $(x).attr('id');
+    if ($('.' + id).hasClass('hidden')) {
+        $('.PermissionGroup').addClass('hidden');
+        $('.' + id).removeClass('hidden');
+        $('.inactive-carrot').removeClass('hidden');
+        $('.active-carrot').addClass('hidden');
+        $(x).children('span.active-carrot').removeClass('hidden');
+        $(x).children('span.inactive-carrot').addClass('hidden');
+    }
+    else {
+        $('.PermissionGroup').addClass("hidden");
+        $('.inactive-carrot').removeClass('hidden');
+        $('.active-carrot').addClass('hidden');
+    }
+
+}
+
+function SizeLayoutElements() {
+    var menuMainHeight = '60';
+    var menuSeondaryMinWidth = '300';
+    var logoFontSize = '40';
+
+    var pageHeight = $(window).height();
+    var bodyUsableHeight = pageHeight - menuMainHeight;
+
+    $('.menu-main-height').height(menuMainHeight + 'px');
+    $('.menu-secondary-container').width(menuSeondaryMinWidth + 'px');
+    $('.menu-secondary-container').height(bodyUsableHeight + 'px');
+    $('.logo').css("font-size", logoFontSize + 'px')
+}
+
+function ResizeLogo() {
+    while ($('.logo').height() > $('.menu-main-height').height()) {
+        var fontSize = parseInt($('.logo').css("font-size"));
+        $('.logo').css("font-size", fontSize - 1 + 'px')
+    }
+}
