5b820cb7d876dc5ba995833ad561d655a1efc89c
houyja <houyja@gmail.com>
2017-05-12 23:22:10 -0400

    Updated Layout V1.5 CSS and JS Changes

--- PerfectInProcess/PerfectInProcess/Style/js/jsLayout.js
+++ PerfectInProcess/PerfectInProcess/Style/js/jsLayout.js
@@ -1,14 +1,4 @@
-﻿$(document).ready(function () {
-    SizeLayoutElements();
-    ResizeLogo();
-})
-
-$(window).resize(function () {
-    SizeLayoutElements();
-    ResizeLogo();
-})
-
-function toggleSecondaryMenu() {
+﻿function toggleSecondaryMenu() {
     $('.menu-togglesidebar-btn').toggleClass("hidden");
     $('.w3-overlay').toggleClass("overlay-toggle-show");
     $('.menu-secondary-container').toggleClass("hidden");
@@ -18,7 +8,6 @@ function toggleSecondaryMenu() {
     $('.active-carrot').addClass('hidden');
 
 }
-
 function toggleSecondaryMenuGroup(x) {
     var id = $(x).attr('id');
     if ($('.' + id).hasClass('hidden')) {
@@ -33,27 +22,5 @@ function toggleSecondaryMenuGroup(x) {
         $('.PermissionGroup').addClass("hidden");
         $('.inactive-carrot').removeClass('hidden');
         $('.active-carrot').addClass('hidden');
-    }
-
-}
-
-function SizeLayoutElements() {
-    var menuMainHeight = '60';
-    var menuSeondaryMinWidth = '300';
-    var logoFontSize = '40';
-
-    var pageHeight = $(window).height();
-    var bodyUsableHeight = pageHeight - menuMainHeight;
-
-    $('.menu-main-height').height(menuMainHeight + 'px');
-    $('.menu-secondary-container').width(menuSeondaryMinWidth + 'px');
-    $('.menu-secondary-container').height(bodyUsableHeight + 'px');
-    $('.logo').css("font-size", logoFontSize + 'px')
-}
-
-function ResizeLogo() {
-    while ($('.logo').height() > $('.menu-main-height').height()) {
-        var fontSize = parseInt($('.logo').css("font-size"));
-        $('.logo').css("font-size", fontSize - 1 + 'px')
     }
 }
